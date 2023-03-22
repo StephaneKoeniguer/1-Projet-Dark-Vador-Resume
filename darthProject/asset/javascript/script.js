2972a15923f1c2e86459851a5f9a7ee750f02c6c
ValentinGuedon <guedonvalentin@gmail.com>
2023-03-22 22:18:00 +0100

    pulled all the branches and merged them locally, standardised the css and made the html fit it, added the favicon across all the pages, almost ready to push on main

---
+++ darthProject/asset/javascript/script.js
@@ -0,0 +1,9 @@
+const animation = window.Typed;
+
+    animation = new Typed ('span',{    
+    strings : ["Je suis Coder", "Je suis Master", "Je suis Father"],
+    loop : true,
+    typeSpeed : 100,
+    backSpeed : 50,
+    loopCount : 10
+    });
